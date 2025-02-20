8871b1fc43f882c6f1a0b588bf53986941dd2029
Sarvesh Vaidhi <sarveshvaidhi06@gmail.com>
2025-02-20 19:40:29 +0530

    aptModel.js

---
+++ models/aptModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const aptSchema = mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    regNo: { type: String, required: true, unique: true },
+    dept: { type: String, required: true },
+    points: { type: Number, required: true, min: 0, max: 50 },
+    aptitude: { type: Number, required: true, min: 0, max: 10 },
+    core: { type: Number, required: true, min: 0, max: 10 },
+    verbal: { type: Number, required: true, min: 0, max: 10 },
+    programming: { type: Number, required: true, min: 0, max: 10 },
+    comprehension: { type: Number, required: true, min: 0, max: 10 },
+  },
+  { collection: "apt" },
+);
+
+module.exports = mongoose.model("apt", aptSchema);
