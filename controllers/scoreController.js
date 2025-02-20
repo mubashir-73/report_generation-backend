a3b8d670222604eb3b95e65117495c1246d71964
Sarvesh Vaidhi <sarveshvaidhi06@gmail.com>
2025-02-20 19:43:29 +0530

    scoreController.js

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -1,15 +1,30 @@
 const asyncHandler = require("express-async-handler");
 const Gd = require("../models/gdModel");
+const apt = require("../models/aptModel")
 
-const getScores = asyncHandler(async (req, res) => {
-  const scores = await Gd.find();
-  console.log(scores);
-  res.status(200).json(scores);
+const getGdScores = asyncHandler(async (req, res) => {
+  const Gdscores = await Gd.find();
+  console.log(Gdscores);
+  res.status(200).json(Gdscores);
 });
 
-const getScore = asyncHandler(async (req, res) => {
-  const score = await apt.find({ email: req.params.id });
-  console.log(score);
-  res.status(200).json(score);
+const getaptScores = asyncHandler(async (req, res) => {
+  const aptscores = await apt.find();
+  console.log(aptscores);
+  res.status(200).json(aptscores);
 });
-module.exports = { getScores, getScore };
+
+const getGdScore = asyncHandler(async (req, res) => {
+  const Gdscore = await Gd.find({ email: req.params.id });
+  console.log(Gdscore);
+  res.status(200).json(Gdscore);
+});
+
+const getaptScore = asyncHandler(async (req, res) => {
+  const aptscore = await apt.find({ email: req.params.id });
+  console.log(aptscore);
+  res.status(200).json(aptscore);
+});
+
+module.exports = { getGdScores, getGdScore, getaptScores, getaptScore };
+
